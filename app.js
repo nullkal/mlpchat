7528923c45ade150adf7f5c2df2f3e7615a800a5
nullkal <nullkal@nil.nu>
2015-06-02 22:51:29 +0900

    add avatar feature

--- app.js
+++ app.js
@@ -9,26 +9,55 @@ var autoprefixer = require('express-autoprefixer');
 app.use(autoprefixer({browsers: 'last 2 versions'}));
 app.use(express.static(__dirname + '/public'));
 
+var playerList = {}; var lastId = 1;
 io.on('connection', function(socket) {
-  var screenName = null;
+  var player;
 
-  socket.on('login', function(name, ack) {
-    screenName = name;
-    ack();
-    io.emit('chat', 'OP', name + 'さんが入室しました。');
-    console.log('LOGIN: ' + name);
+  socket.on('login', function(p, ack) {
+    player = p;
+    player.id = lastId++;
+    ack(player.id);
+    for (var i in playerList) {
+      socket.emit('player add', playerList[i]);
+    }
+    playerList[player.id] = player;
+    socket.broadcast.emit('player add', player);
+    io.emit('chat', {name: 'OP'}, player['name'] + 'さんが入室しました。');
+    console.log('LOGIN: ' + player['name']);
   });
 
   socket.on('chat', function(msg){
-    if (screenName) {
-      io.emit('chat', screenName, msg);
+    if (player) {
+      io.emit('chat', player, msg);
+    }
+  });
+
+  socket.on('player move', function(p){
+    if (player) {
+      player['x'] = p['x'];
+      player['y'] = p['y'];
+      player['flipped'] = p['flipped'];
+      player['state'] = p['state'];
+      socket.broadcast.emit('player move', player);
+    }
+  });
+
+  socket.on('player stop', function(p){
+    if (player) {
+      player['x'] = p['x'];
+      player['y'] = p['y'];
+      player['flipped'] = p['flipped'];
+      player['state'] = p['state'];
+      socket.broadcast.emit('player stop', player);
     }
   });
 
   socket.on('disconnect', function() {
-    if (screenName) {
-      io.emit('chat', 'OP', screenName + 'さんが退室しました。');
-    console.log('LOGOUT: ' + screenName);
+    if (player) {
+      io.emit('chat', {name: 'OP'}, player['name'] + 'さんが退室しました。');
+      delete playerList[player.id];
+      socket.broadcast.emit('player remove', player);
+      console.log('LOGOUT: ' + player['name']);
     }
   });
 });
--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-
 var Scene = (function() {
   var currentScene = null;
 
@@ -7,17 +6,17 @@ var Scene = (function() {
     self.name = name;
     self.initialize = initialize;
     self.finalize = null;
-    intervals = [];
+    self.intervals = [];
 
     self.setInterval = function() {
-      intervals.push(setInterval.apply(window, arguments));
+      self.intervals.push(setInterval.apply(window, arguments));
       return this;
     }
     self.clearAllIntervals = function() {
-      for (var i = 0; i < intervals.length; i++) {
-        clearInterval(intervals[i]);
+      for (var i = 0; i < self.intervals.length; i++) {
+        clearInterval(self.intervals[i]);
       }
-      intervals = [];
+      self.intervals = [];
       return this;
     }
     self.show = function() {
@@ -25,8 +24,8 @@ var Scene = (function() {
       if (currentScene) {
         $('#scene_' + this.name).fadeIn(200);
         $('#scene_' + currentScene.name).fadeOut(200, function() {
-          currentScene.clearAllIntervals();
-        });
+          this.clearAllIntervals();
+        }.bind(currentScene));
         if (currentScene.finalize) {
           currentScene.finalize();
         }
@@ -47,28 +46,73 @@ var Scene = (function() {
   }
 })();
 
-function drawCanvas() {
-  var canvas = $('#canvas')[0];
-  var ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+var Pony = (function() {
+  return function(opts) {
+    var self = opts;
+    var img = $('<img>')
+      .addClass('player__image')
+      .attr('src', 'img/' + self.avatar + '.gif');
+    var playerInfo = $('<div>')
+      .addClass('player__info')
+      .text(self.name);
+    var elem = $('<div>')
+      .append(playerInfo)
+      .append(img);
+    $('#players').append(elem);
+    if (!('id' in self)) {
+      self.id = 0;
+    }
+    if (!('x' in self)) {
+      self.x = 0;
+    }
+    if (!('y' in self)) {
+      self.y = 0;
+    }
+    if (!('state' in self)) {
+      self.state = 'stopped';
+    }
+    if (!('flipped' in self)) {
+      self.flipped = false;
+    }
+    self.finalize = function() {
+      elem.remove();
+    }
+    self.refresh = function() {
+      var unit = $(window).height() * 0.2;
+      var real_x = $(window).width()  / 2   + (self.x - 0.5) * unit;
+      var real_y = $(window).height() * 0.92 - (self.y + 1  ) * unit;
+      elem
+        .css('left', real_x + 'px')
+        .css('top' , real_y + 'px');
+      if (self.state != self.prevState) {
+        if (self.state == 'moving') {
+          img.attr('src', 'img/' + self.avatar + '_trotting.gif'); 
+        } else {
+          img.attr('src', 'img/' + self.avatar + '.gif'); 
+        }
+        self.prevState = self.state;
+      }
+      if (self.flipped) {
+        img.addClass('player--flipped');
+      } else {
+        img.removeClass('player--flipped');
+      }
+    }
+    return self;
+  } 
+})();
 
-function updateCanvasSize() {
-  var width = $(window).width();
-  var height = $(window).height();
-
-  var canvas = $('#canvas');
-  canvas.attr('width', width);
-  canvas.attr('height', height);
-
-  drawCanvas();
+function randomAvatar() {
+  var avatars = [
+    'twilight-sparkle',
+    'sunset-shimmer',
+    'ak-yearling',
+    'applejack'
+  ];
+  return avatars[Math.floor(Math.random() * avatars.length)];
 }
 
 $(function() {
-  updateCanvasSize();
-  interval = setInterval(drawCanvas, 16);
-  $(window).resize(updateCanvasSize);
-
   var scene_loginForm = Scene('login-form', function() {
     var screenName = localStorage.getItem('screen-name');
     if (screenName) {
@@ -88,7 +132,24 @@ $(function() {
 
   var scene_chat = Scene('chat', function(screenName) {
     $('#messages').empty();
+    $('#players').empty();
     $('#chat-text-input').val('');
+    scene_chat.me = Pony({name: screenName, avatar: randomAvatar()});
+    scene_chat.players = {};
+    scene_chat.setInterval(function() {
+      for (var i in scene_chat.players) {
+        if (scene_chat.players[i].state == 'moving') {
+          if (scene_chat.players[i].flipped) {
+            scene_chat.players[i].x -= 0.02;
+          } else {
+            scene_chat.players[i].x += 0.02;
+          }
+        }
+        scene_chat.players[i].refresh();
+      }
+    }, 16);
+    var holding_left  = false;
+    var holding_right = false;
     $(window).on('keydown', function(evt) {
       if (evt.which == 84) {
         if (!$('#chat-text-input').is(':focus')) {
@@ -98,8 +159,39 @@ $(function() {
       } else if (evt.which == 27 || evt.ctrlKey && evt.which == 219) {
         $('#chat-text-input').val('');
         $('#chat-text-input').blur();
+      } else if (evt.which == 37 && !holding_left) {
+        scene_chat.me.state = 'moving';
+        scene_chat.me.flipped = true;
+        scene_chat.socket.emit('player move', scene_chat.me);
+        holding_left = true;
+      } else if (evt.which == 39 && !holding_right) {
+        scene_chat.me.state = 'moving';
+        scene_chat.me.flipped = false;
+        scene_chat.socket.emit('player move', scene_chat.me);
+        holding_right = true;
       }
     });
+    $(window).on('keyup', function(evt) {
+      if (evt.which == 37) {
+        scene_chat.me.state = 'stopped';
+        scene_chat.socket.emit('player stop', scene_chat.me);
+        holding_left = false;
+        if (holding_right) {
+          scene_chat.me.state = 'moving';
+          scene_chat.me.flipped = false;
+          scene_chat.socket.emit('player move', scene_chat.me);
+        }
+      } else if (evt.which == 39) {
+        scene_chat.me.state = 'stopped';
+        scene_chat.socket.emit('player stop', scene_chat.me);
+        holding_right = false;
+        if (holding_left) {
+          scene_chat.me.state = 'moving';
+          scene_chat.me.flipped = true;
+          scene_chat.socket.emit('player move', scene_chat.me);
+        }
+      }
+    })
     $('#chat-form').on('submit', function() {
       var chatInput = $('#chat-text-input');
       if (chatInput.val() != '') {
@@ -113,15 +205,16 @@ $(function() {
       scene_chat.socket.disconnect();
       scene_chat.socket.removeAllListeners();
       $(window).off('keydown');
+      $(window).off('keyup');
       $('#scene_chat *').off();
     }
     return new Promise(function(resolve, reject) {
       scene_chat.socket = io({multiplex: false});
       scene_chat.socket.connect();
-      scene_chat.socket.on('chat', function(screenName, msg) {
+      scene_chat.socket.on('chat', function(p, msg) {
         var toast = $('<div>');
         toast.addClass('messages__msg');
-        toast.append($('<strong>').text(screenName));
+        toast.append($('<strong>').text(p['name']));
         toast.append(document.createTextNode(': ' + msg));
         $('#messages').append(toast);
         toast.hide();
@@ -135,7 +228,28 @@ $(function() {
       scene_chat.socket.on('disconnect', function() {
         scene_loginForm.show();       
       });
-      scene_chat.socket.emit('login', screenName, function() {
+      scene_chat.socket.on('player add', function(p) {
+        scene_chat.players[p.id] = Pony(p);
+      });
+      scene_chat.socket.on('player remove', function(p) {
+        scene_chat.players[p.id].finalize();
+        delete scene_chat.players[p.id];
+      });
+      scene_chat.socket.on('player move', function(p) {
+        scene_chat.players[p.id].state = p.state;
+        scene_chat.players[p.id].flipped = p.flipped;
+        scene_chat.players[p.id].x = p.x;
+        scene_chat.players[p.id].y = p.y;
+      });
+      scene_chat.socket.on('player stop', function(p) {
+        scene_chat.players[p.id].state = p.state;
+        scene_chat.players[p.id].flipped = p.flipped;
+        scene_chat.players[p.id].x = p.x;
+        scene_chat.players[p.id].y = p.y;
+      });
+      scene_chat.socket.emit('login', scene_chat.me, function(id) {
+        scene_chat.me.id = id;
+        scene_chat.players[id] = scene_chat.me;
         resolve();
       });
     });
